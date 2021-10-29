a7a66c6c025b6f34e7889284b56971486f74ed35
Abdullah Tariq <abdullah.tariq@square63.org>
2021-10-29 17:48:37 +0500

    Added form validation using regex, Created function to generate random username, Removed user_name index, Added username as hidden field,Added all tracable columns, Beautified SignIn/SingUp forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  validates :user_name, format: { with: /\A(?=.*?[#?!@$%^&*-]).{4,30}\z/ , message: "User name Requirements not met" }
+  validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d).{8,}\z/, message: "Password Requirements not met" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_105001) do
+ActiveRecord::Schema.define(version: 2021_10_29_074927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 2021_10_27_105001) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end

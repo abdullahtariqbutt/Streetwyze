0d6030f48db5505ed936fa94bcdc9a06c54d9252
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-01 12:56:58 +0500

    Renamed Assests to Assest in all related files and folders

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,49 @@
+class AssetsController < ApplicationController
+  before_action :set_asset, only: %i[ show edit update destroy ]
+
+  def index
+    @assets = Asset.all
+  end
+
+  def show
+  end
+
+  def new
+    @asset = asset.new
+  end
+
+  def edit
+  end
+
+  def create
+    @asset = asset.new(asset_params)
+
+    if @asset.save
+      redirect_to @asset
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @asset.update(asset_params)
+      redirect_to @asset
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @asset.destroy
+    redirect_to assets_url
+  end
+
+  private
+    def set_asset
+      @asset = asset.find(params[:id])
+    end
+
+    def asset_params
+      params.require(:asset).permit(:name, :address, :category, :leave_rating, :rating, :stuff_type, :story)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_071309) do
+ActiveRecord::Schema.define(version: 2021_11_01_075313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "assests", force: :cascade do |t|
+  create_table "assets", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"

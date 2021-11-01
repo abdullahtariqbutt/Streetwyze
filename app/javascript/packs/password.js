fd8a078aaab404ccbfec4785f4df5addac61295e
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-01 11:30:36 +0500

    Edited password.js and added show/hide function to SignUp form

--- app/javascript/packs/password.js
+++ app/javascript/packs/password.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
-  $("#eye").on("click", function(){
-    var x = document.getElementById("password");
-    var show_eye = document.getElementById("show_eye");
-	var hide_eye = document.getElementById("hide_eye");
 
-	hide_eye.classList.remove("d-none");
+	var signin_eye = document.getElementById("signIn_pswd");
+	var eye = document.getElementById("signUp_pswd");
+	var confirm_eye = document.getElementById("signUp_c_pswd");
 
-	if (x.type === "password"){
-	  x.type = "text";
-	  show_eye.style.display = "none";
-	  hide_eye.style.display = "block";
-	}
-	else{
-	  x.type = "password";
-	  show_eye.style.display = "block";
-	  hide_eye.style.display = "none";
-	}
-  });	
+	var x = ""
+	var show_eye = ""
+	var hide_eye = ""
+
+	$("#eye").on("click", function(){
+		x = document.getElementById("password");
+  	show_eye = document.getElementById("show_eye");
+		hide_eye = document.getElementById("hide_eye");
+
+		hide_eye.classList.remove("d-none");
+
+		if (x.type === "password"){
+	  	x.type = "text";
+	  	show_eye.style.display = "none";
+	  	hide_eye.style.display = "block";
+		}
+		else{
+	  	x.type = "password";
+	  	show_eye.style.display = "block";
+	  	hide_eye.style.display = "none";
+		}
+  });
+
+  $("#c_eye").on("click", function(){
+		x = document.getElementById("confirm_password");
+  	show_eye = document.getElementById("c_show_eye");
+		hide_eye = document.getElementById("c_hide_eye");
+
+		hide_eye.classList.remove("d-none");
+
+		if (x.type === "password"){
+	  	x.type = "text";
+	  	show_eye.style.display = "none";
+	  	hide_eye.style.display = "block";
+		}
+		else{
+	  	x.type = "password";
+	  	show_eye.style.display = "block";
+	  	hide_eye.style.display = "none";
+		}
+  });
+
 });

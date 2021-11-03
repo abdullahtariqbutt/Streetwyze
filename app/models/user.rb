d735c42a9e1675b686db64c4ce8628f1e4a32032
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-03 17:17:34 +0500

    Formatted code, Deleted stylesheet folder, added constant for regex in user model and remove style pack link form application.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
 
+  USERNAME_REGEX = "/\A(?=.*?[#?!@$%^&*-]).{4,30}\z/"
+  PASSWORD_REGEX = "/\A(?=.*[A-Za-z])(?=.*\d).{8,}\z/"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
 
-  validates :user_name, format: { with: /\A(?=.*?[#?!@$%^&*-]).{4,30}\z/ , message: " must have one special character and only have a max of 30 characters" }
-  validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d).{8,}\z/, message: "should be minimum 8 characters long with atleast one Capital and one Special character" }
+  validates :user_name, format: { with: USERNAME_REGEX , message: " must have one special character and only have a max of 30 characters" }
+  validates :password, format: { with: PASSWORD_REGEX, message: "should be minimum 8 characters long with atleast one Capital and one Special character" }
   
 end

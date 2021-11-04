8fee3b9fb832f867c967380c90cb9a3c3fb33504
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-04 12:04:12 +0500

    Added form and js code to display added assets beautifully

--- app/javascript/packs/rating.js
+++ app/javascript/packs/rating.js
@@ -14,16 +14,25 @@ $(document).on('turbolinks:load',function(){
 
 	$("#rating_check").on("click", function(){
 
-      $(this).text('I don\'t want to leave a rating');
-      $("#rate").hide("slow");
+    $(this).text('I don\'t want to leave a rating');
+    $("#rate").hide("slow");
 
-      $('#rating').raty({
-
-      	targetType: 'score',
-        targetKeep: true,
-        target: '#review_star',
-        targetScore: null
-      });
-
+    $('#rating').raty({
+    	targetType: 'score',
+      targetKeep: true,
+      target: '#review_star',
+      targetScore: null
     });
+
+  });
+
+  $('#rating_display').raty({
+
+    readOnly: true,
+
+    starOn: '../images/star-on.png',
+    starOff: '../images/star-off.png',
+
+  });
+
 });

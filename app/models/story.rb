8d0753d0cf49b113fa8bb7dae9e8a70314518a6d
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-05 11:05:24 +0500

    Added Stories Model, Edited Resources, Modified links in navbar and views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,8 @@
-class MapAsset < ApplicationRecord
+class Story < ApplicationRecord
 	has_rich_text :story
 	has_many_attached :images
 
-	has_many :stories
+	belongs_to :map_assets
 
 	validates :name, :address, :category, :stuff_type, presence: true
 	validates :story, length: { maximum: 2000, too_long: "%{count} characters is the maximum allowed" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :map_assets
+
+  resources :map_assets do
+    resources :stories, only: [ :new, :create]
+  end
+  resources :stories, only: [:index, :show, :edit, :update, :destroy]
 
   devise_for :users
   root to: "users#index"
+
 end

68bdf9d178b2f6577f6bc9ddb37f052d01b67f2d
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-18 15:47:57 +0500

    Added comments to Stimulus JS code

--- app/javascript/controllers/dynamic_select_controller.js
+++ app/javascript/controllers/dynamic_select_controller.js
@@ -7,12 +7,14 @@ export default class extends Controller {
     this.selected()
   }
 
+  // All items would be hidden at first and will only be visible/ removed hidden class form it
+  // when relavent option will be slected
   selected() {
     this.hideFields()
     switch (this.selectTarget.value) {
-      case 'smiley_based':
-        this.choiceTarget.classList.remove('hidden')
-        break;
+      // case 'smiley_based':
+      //   this.choiceTarget.classList.remove('hidden')
+      //   break;
       case 'multiple_choice':
         this.choiceTarget.classList.remove('hidden')
         break;
@@ -22,8 +24,13 @@ export default class extends Controller {
     }
   }
 
+  // Fields hidden at first
   hideFields() {
+    // content for multiple choice hidden
     this.choiceTarget.classList.add('hidden')
+    // content for open ended question hidden
     this.longTarget.classList.add('hidden')
+    // content for simley based hidden
+    // this.longTarget.classList.add('hidden')
   }
 }
--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -3,16 +3,23 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["add_item", "template"]
 
+  // Code to add new Question or Answer
   add_association(event) {
     event.preventDefault()
+    // Get the template defined in form view in <template> tags and add a unique id with it using time
     var content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().getTime())
+    // Add the template to the page
     this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
   }
 
+  // Code to remove clicked Question ors Answer
   remove_association(event) {
     event.preventDefault()
+    // Get the closest div with nested-fields class
     let item = event.target.closest(".nested-fields")
+    // Pass a true check to allow destruction of that item
     item.querySelector("input[name*='_destroy']").value = 1
+    // Also Hide it as it will be destroyed later and we should not see it when removed
     item.style.display = 'none'
   }
 }

611ed32b57842c01a118d25e8b48f6f36beab9a8
Abdullah Tariq <abdullah.tariq@square63.org>
2021-11-30 16:08:31 +0500

    Fixed some PR issues

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,25 +1,25 @@
 class CheckoutController < ApplicationController
 
   def create
-  	user = User.find(params[:id])
-  	@session = Stripe::Checkout::Session.create({
-		  payment_method_types: ['card'],
-		  line_items: [{
-		  	name:user.user_name,
-		  	amount: 999,
-		  	currency:"usd",
-		  	quantity:1,
-		  }],
-		 	client_reference_id: user.id,
+    user = User.find(params[:id])
+    @session = Stripe::Checkout::Session.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        name:user.user_name,
+        amount: 999,
+        currency:"usd",
+        quantity:1,
+      }],
+      client_reference_id: user.id,
 
-		  mode: 'payment',
-		  success_url: 'http://localhost:3000/users/edit',
-		  cancel_url: 'http://localhost:3000/users/edit',
+      mode: 'payment',
+      success_url: 'http://localhost:3000/users/edit',
+      cancel_url: 'http://localhost:3000/users/edit',
 	})
 
-		respond_to do |format|
-		  format.js
-		end
+    respond_to do |format|
+      format.js
+    end
   end
 
 end
